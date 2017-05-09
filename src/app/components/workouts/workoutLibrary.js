0bafee860b1c67ee49d691ac45cbf3b38968fc90
mike-stumpf <mike.a.stumpf@gmail.com>
2017-05-09 09:29:29 +0100

    workouts - added workout library route and template

---
+++ src/app/components/workouts/workoutLibrary.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class WorkoutLibrary extends Component {
+    render() {
+        return (
+            <main id="container-profile" className="grid-container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <p>
+                            Workout Library
+                        </p>
+                    </div>
+                </div>
+            </main>
+        );
+    }
+}
+
+export default WorkoutLibrary;
--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { Router, Route, browserHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
-import store from './store'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import store from './store';
 import MiddlewareService from './app/services/middleware';
 
 // Layouts
@@ -14,6 +14,7 @@ import DashboardContainer from './app/components/dashboard/dashboardContainer';
 import NotFound from './app/components/shared/notFound';
 import WorkoutContainer from './app/components/workouts/workoutContainer';
 import ProfileContainer from './app/components/profile/profileContainer';
+import WorkoutLibraryContainer from './app/components/workouts/workoutLibrary';
 
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
@@ -24,8 +25,9 @@ export default (
             <Route path="/" name="login" component={LoginContainer} />
             <Route component={MainLayout} onEnter={MiddlewareService.isUserLoggedInMiddleware}>
                 <Route path="/dashboard" name="dashboard" component={DashboardContainer}/>
-                <Route path="/profile" name="dashboard" component={ProfileContainer}/>
-                <Route path="/workout" name="dashboard" component={WorkoutContainer}/>
+                <Route path="/profile" name="profile" component={ProfileContainer}/>
+                <Route path="/workout" name="workout" component={WorkoutContainer}/>
+                <Route path="/workouts" name="workout-library" component={WorkoutLibraryContainer}/>
                 //misc
                 <Route path='*' component={NotFound} />
             </Route>

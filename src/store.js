381cf3958644209bcf1b20371b076c83d9dba095
mike-stumpf <mike.a.stumpf@gmail.com>
2017-05-09 09:03:23 +0100

    core - added persistent storage for dev environments

--- src/store.js
+++ src/store.js
@@ -1,16 +1,28 @@
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import { applyMiddleware, createStore } from 'redux';
-
-//todo, store state to local storage?
+import { applyMiddleware, createStore, compose } from 'redux';
+import { autoRehydrate, persistStore } from 'redux-persist';
 
 import reducers from './app/ducks/index'
+import helperService from './app/services/helper';
 
 const middleware = applyMiddleware(thunk, createLogger());
+let store;
 
-const store = createStore(
-    reducers,
-    middleware
-);
+if (helperService.isDevelopment()){
+    //persist storage only for dev environment for now
+    store = compose(
+        middleware,
+        autoRehydrate()
+    )(createStore)(reducers);
+
+    persistStore(store);
+//todo expire cache https://github.com/rt2zz/redux-persist/issues/65
+} else {
+    store = createStore(
+        reducers,
+        middleware
+    );
+}
 
 export default store;

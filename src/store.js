0ddaf9b289cfef43d73e2b56953c74ad0e90a500
mike-stumpf <mike.a.stumpf@gmail.com>
2017-05-18 10:35:48 +0100

    core - expanded storage persistence to live environments

--- src/store.js
+++ src/store.js
@@ -4,35 +4,26 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import { autoRehydrate, persistStore } from 'redux-persist';
 
 import reducers from './app/ducks/index'
-import helperService from './app/services/helper';
 import { actions as userActions } from './app/ducks/user';
 
 const middleware = applyMiddleware(thunk, createLogger());
-let store;
 
-if (helperService.isDevelopment()){
-    store = compose(
-        middleware,
-        autoRehydrate()
-    )(createStore)(reducers);
+let store = compose(
+    middleware,
+    autoRehydrate()
+)(createStore)(reducers);
 
-    persistStore(store, {}, () => {
-        let currentState = store.getState(),
-            sessionToken = currentState.userState.sessionToken;
-        if (!!sessionToken){
-            userActions.getProfile()
-                .catch(() => {
-                    //purge store upon session cookie expiry
-                    persistStore(store).purge();
-                    return store.dispatch(userActions.logout());
-                });
-        }
-    });
-} else {
-    store = createStore(
-        reducers,
-        middleware
-    );
-}
+persistStore(store, {}, () => {
+    let currentState = store.getState(),
+        sessionToken = currentState.userState.sessionToken;
+    if (!!sessionToken){
+        userActions.getProfile()
+            .catch(() => {
+                //purge store upon session cookie expiry
+                persistStore(store).purge();
+                return store.dispatch(userActions.logout());
+            });
+    }
+});
 
 export default store;
